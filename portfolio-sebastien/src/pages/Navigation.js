b5308a846e4468b5298732684046cd29b33c3006
Firouzfar <firouzfar.sebastien@gmail.com>
2021-05-17 17:37:24 +0200

    Update navbar and install firebase package / modification de la navbar et installation du package firebase

--- portfolio-sebastien/src/pages/Navigation.js
+++ portfolio-sebastien/src/pages/Navigation.js
@@ -4,16 +4,7 @@ import { NavLink } from "react-router-dom";
 export const Navigation = () => {
   return (
     <div className="container">
-      {/* <nav className="navbar navbar-expand-md navbar-expand-lg text-aligne">
-        <ul className="navbar-nav">
-          <li><NavLink exact to="/" activeClassName="nav-active"> Accueil</NavLink></li>
-          <li><NavLink exact to="/about" activeClassName="nav-active">About</NavLink></li>
-          <li><NavLink exact to="/projects" activeClassName="nav-active">Projects</NavLink></li>
-          <li><NavLink exact to="/contact" activeClassName="nav-active">Contact</NavLink></li>
-          </ul>
-      </nav> */}
-
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <nav class="navbar navbar-expand-lg navbar-light ">
         <button
           class="navbar-toggler"
           type="button"
