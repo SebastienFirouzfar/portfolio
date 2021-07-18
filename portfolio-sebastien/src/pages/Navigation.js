8d7fe2dcd4982712d78df9c802fbb9fea3b23bcf
Firouzfar <firouzfar.sebastien@gmail.com>
2021-07-18 21:46:28 +0200

    Update the error / modifier l'erreur

--- portfolio-sebastien/src/pages/Navigation.js
+++ portfolio-sebastien/src/pages/Navigation.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
+import {Navbar, Container, Nav } from 'react-bootstrap';
 
 export const Navigation = () => {
   return (
     <div className="containerNavigation">
-      <nav className="navbar navbar-expand-lg navbar-light ">
+      {/* <nav className="navbar navbar-expand-lg navbar-light ">
         <button
           className="navbar-toggler"
           type="button"
@@ -43,7 +44,22 @@ export const Navigation = () => {
             </li>
           </ul>
         </div>
-      </nav>
+      </nav> */}
+
+      <Navbar expand="lg">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+              <Nav.Link href="/projects">Project</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+              <Nav.Link href="./media/Firouzfar-Sebastien-CV-Dev-Web.pdf" target="_blank">Download CV</Nav.Link> 
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 };
