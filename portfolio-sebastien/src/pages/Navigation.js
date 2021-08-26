f1185a76bcc097727e5a011b874338f44366cf34
Firouzfar <firouzfar.sebastien@gmail.com>
2021-08-26 15:27:26 +0200

    Update my cv / modification de mon cv

--- portfolio-sebastien/src/pages/Navigation.js
+++ portfolio-sebastien/src/pages/Navigation.js
@@ -14,7 +14,7 @@ export const Navigation = () => {
               <Nav.Link href="/about">About</Nav.Link>
               <Nav.Link href="/projects">Project</Nav.Link>
               <Nav.Link href="/contact">Contact</Nav.Link>
-              <Nav.Link href="./media/Firouzfar-Sebastien-CV-18.08.21.pdf" target="_blank">Download CV</Nav.Link> 
+              <Nav.Link href="./media/Firouzfar-Sebastien-CV-25.08.21.pdf" target="_blank">Download CV</Nav.Link> 
             </Nav>
           </Navbar.Collapse>
         </Container>
